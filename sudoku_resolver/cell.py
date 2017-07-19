0aaa4005130d1c9da4adac389fd44931f2691db6
Diego França <diegobmf@gmail.com>
2017-07-19 16:12:49 -0300

    Changed the possibilities to strings

--- sudoku_resolver/cell.py
+++ sudoku_resolver/cell.py
@@ -2,7 +2,7 @@ class Cell:
     """ class for the cells in the sudoku grid """
     def __init__(self):
         self.value = "X"
-        self.possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.possibilities = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     def change_possibility(self, poss):
         # function to set a possibility of a number to false
